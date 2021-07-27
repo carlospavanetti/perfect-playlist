3d52eeb0c35ecccc7a37054202ad5e682ec89c02
Carlos Pavanetti <carlos.pavanetti@buser.com.br>
2021-07-26 22:43:28 -0300

    chore: init spring-boot project

---
+++ src/main/java/dev/carlospavanetti/perfectplaylist/PerfectPlaylistApplication.java
@@ -0,0 +1,13 @@
+package dev.carlospavanetti.perfectplaylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfectPlaylistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfectPlaylistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/carlospavanetti/perfectplaylist/PerfectPlaylistApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.carlospavanetti.perfectplaylist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfectPlaylistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
